e68756ae8fc9a32f23cf52231203b6730f8e4904
Jocarl Basco <jocarlbasco24@gmail.com>
2024-01-24 15:42:10 +0800

    Dev 38: System Admin Additional

--- decorators/rate_decorators.py
+++ decorators/rate_decorators.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from datetime import datetime, timedelta
 from functools import wraps
+from models import db, RateLimit
 
 request_history = {}
 
@@ -10,27 +11,49 @@ def resend_otp_decorator(message):
 def login_decorator(message):
     return rate_limit_decorator(limit=3, period=60*5, message=message)
 
+def cleanup_expired_ips():
+    current_time = datetime.now()
+    RateLimit.query.filter(RateLimit.expiration_timestamp < current_time).delete()
+    db.session.commit()
 
-
-# Custom rate limiter decorator with dynamic parameters
 def rate_limit_decorator(limit, period, message):
     def decorator(func):
-        @wraps(func)
         def wrapper(*args, **kwargs):
+            # Clean up expired IP addresses
+            cleanup_expired_ips()
+
             ip_address = request.remote_addr
             print('IP ADD: ', ip_address)
-            # Check if the IP address is in the request_history dictionary
-            if ip_address in request_history:
-                # Get the timestamp of the last request
-                last_request_time = request_history[ip_address]
+
+            # Check if the IP address is in the database
+            rate_limit_entry = RateLimit.query.filter_by(ip_address=ip_address).first()
+
+            if rate_limit_entry:
+                # Entry exists, check for rate limit
+                request_count = rate_limit_entry.request_count
+                print("LIMIT VALUE: ", request_count)
+                print("LAST REQUEST: ", rate_limit_entry.last_request_timestamp)
 
                 # Check if the time difference is within the limit_period
-                if datetime.now() - last_request_time < timedelta(seconds=period):
-                    print("REACH DECORATOR")
-                    return jsonify({"error": True, "message": message}), 429
+                if datetime.now() - rate_limit_entry.last_request_timestamp < timedelta(seconds=period):
+                    # Check if the number of requests is within the limit
+                    if request_count >= limit:
+                        print("LIMIT REACHED")
+                        return jsonify({"error": True, "message": message}), 429
+                    else:
+                        # Increment the request count
+                        rate_limit_entry.request_count += 1
+                else:
+                    # Reset the request count if the period has passed
+                    rate_limit_entry.request_count = 1
+                    rate_limit_entry.last_request_timestamp = datetime.now()
+            else:
+                # Initialize the rate limit entry for a new IP address
+                expiration_timestamp = datetime.now() + timedelta(seconds=period)
+                rate_limit_entry = RateLimit(ip_address=ip_address, request_count=1, expiration_timestamp=expiration_timestamp)
+                db.session.add(rate_limit_entry)
 
-            # Update the request_history with the current timestamp
-            request_history[ip_address] = datetime.now()
+            db.session.commit()
 
             # Call the original function
             return func(*args, **kwargs)
