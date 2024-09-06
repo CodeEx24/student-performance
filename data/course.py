6ddd9ccabab33ca6f4ff131e198290e452309632
Jocarl Basco <76276243+CodeEx24@users.noreply.github.com>
2024-09-06 15:04:43 +0800

    Dev 63: Login UI Button Updates

--- data/course.py
+++ data/course.py
@@ -2,36 +2,42 @@
 course_data = [
   {
     "CourseId": 1,
+    "DurationYears": 4,
     "CourseCode": "BBTE",
     "Name": "Bachelor in Business Teacher Education",
     "Description": "This program prepares students to become educators in the field of business education."
   },
   {
     "CourseId": 2,
+    "DurationYears": 4,
     "CourseCode": "BBA",
     "Name": "Bachelor of Business Administration",
     "Description": "A comprehensive business program that covers various aspects of administration and management."
   },
   {
     "CourseId": 3,
+    "DurationYears": 4,
     "CourseCode": "BPAM",
     "Name": "Bachelor of Public Administration major in Public Financial Management",
     "Description": "Focuses on equipping students with skills to manage public finances and resources."
   },
   {
     "CourseId": 4,
+    "DurationYears": 4,
     "CourseCode": "BSBA-MM",
     "Name": "Bachelor of Science Business Administration Major in Marketing",
     "Description": "A comprehensive business program that covers various aspects of administration and management."
   },
   {
     "CourseId": 5,
+    "DurationYears": 4,
     "CourseCode": "BTLE-HE",
     "Name": "Bachelor of Business Tecchnology and Livelihood Education Major in Home Economics",
     "Description": "A comprehensive business program that covers various aspects of administration and management."
   },
   {
     "CourseId": 7,
+    "DurationYears": 4,
     "CourseCode": "BSIT",
     "Name": "Bachelor of Science in Information Technology",
     "Description": "Focuses on various aspects of information technology and software development."
